cd36e2815fa0d2400a692146b2fd2fee8c539d6a
mxzpytlk <mxzpytlk@gmail.com>
2021-10-03 10:27:35 +0300

    getting port from environment

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './filters/exception.filter';
 import * as cookieParser from 'cookie-parser';
 
+const PORT = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
   app.use(cookieParser());
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();

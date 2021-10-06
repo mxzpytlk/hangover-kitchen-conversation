bed56121c304343b1e4e7fd93bb1d8c3b3aa2975
mxzpytlk <mxzpytlk@gmail.com>
2021-10-07 01:22:09 +0300

    Add notification service to roomsService dependencies

--- src/modules/web/room/room.module.ts
+++ src/modules/web/room/room.module.ts
@@ -1,26 +1,34 @@
 import { Module } from '@nestjs/common';
+import { INotificationPort } from 'src/domain/notifications/out/notification.port';
 import { RoomServiceSymbol } from 'src/domain/rooms/in/rooms.use-case';
 import { RoomService } from 'src/domain/rooms/services/room.service';
 import { DatabaseModule } from 'src/modules/database/database.module';
 import { RoomPersistanceAdapter } from 'src/modules/database/user-room-orm/adapters/room-persistance.adapter';
 import { UserRoomPersistanceAdapter } from 'src/modules/database/user-room-orm/adapters/user-room-persistance.adapter';
+import { NotificationModule } from '../notification/notification.module';
+import { NotificationServiceSymbol } from '../notification/services/notification.service';
 import { RoomResolver } from './graphql/room.resolver';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, NotificationModule],
   providers: [
     {
       provide: RoomServiceSymbol,
       useFactory: (
         roomPersistanceAdapter: RoomPersistanceAdapter,
         userRoomPersistanceAdapter: UserRoomPersistanceAdapter,
+        notificationPort: INotificationPort,
       ) =>
         new RoomService(
           roomPersistanceAdapter,
           userRoomPersistanceAdapter,
-          null,
+          notificationPort,
         ),
-      inject: [RoomPersistanceAdapter, UserRoomPersistanceAdapter],
+      inject: [
+        RoomPersistanceAdapter,
+        UserRoomPersistanceAdapter,
+        NotificationServiceSymbol,
+      ],
     },
     RoomResolver,
   ],
